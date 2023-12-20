15a5bdf15fd82e1aa9c524a27ee312e477cb95a6
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-12-20 15:22:44 +0100

    update

---
+++ lib/pages/student_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StudentPage extends StatelessWidget {
+  const StudentPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,  
+        title: const Text("Un étudiant"),
+      ),
+    );
+  }
+}
